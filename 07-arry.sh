57d789f4b64d0699555f40d115add881f1c6079a
suresh228devops <suresh228devops@gmail.com>
2025-05-16 00:07:18 +0530

    up to 07-arry.sh files added

---
+++ 07-arry.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MOVIES=("Count" "HIT2" "PUSHPA2" "THANDEL")
+
+echo "First Movie :: ${MOVIES[0]}"
+echo "Second Movie :: ${MOVIES[1]}"
+echo "Third Movie :: ${MOVIES[2]}"
+echo "Fourth Movie :: ${MOVIES[4]}"
+
+echo "FIFTH Movie :: ${MOVIES[4]}"
+
+
+
+echo "All movies: ${MOVIES[@]"
+
+
